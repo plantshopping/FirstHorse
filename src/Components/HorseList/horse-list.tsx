5820805de124eb1f051fcb6ca7d56af244417648
Amy Lin <xiaohui@outlook.co.nz>
2021-09-25 12:31:01 +1200

    Create and display horse list template

---
+++ src/Components/HorseList/horse-list.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from "react";
+
+interface Props {}
+
+export default function HorseList({}: Props): ReactElement {
+  return <div>TODO Horses will show here</div>;
+}
